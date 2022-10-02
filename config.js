c30809e673eaea6321d11dd13690456238a64f1f
M ramdan Ismail <91749019+MRamdan29@users.noreply.github.com>
2022-10-03 06:59:26 +0700

    Update config.js

--- config.js
+++ config.js
@@ -19,11 +19,11 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['6285875158363']
-global.ownernomer = "6285875158363"
-global.premium = ['6285875158363']
-global.packname = 'Sticker  Naze'
-global.author = 'Naze'
+global.owner = ['6285798048936']
+global.ownernomer = "6283867102010"
+global.premium = ['6285798048936']
+global.packname = 'FR BO MD'
+global.author = 'RAMDAN'
 global.sessionName = 'nazedev'  //jangan diganti bro nanti error
 global.prefa = ['','!','.','#','&']
 global.sp = ''
